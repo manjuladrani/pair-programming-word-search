49609a8a9f916f1d27c199045e34641f2bda0114
manjuladrani <88101950+manjuladrani@users.noreply.github.com>
2021-09-22 18:29:10 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
+
+const transpose = function(matrix) {
+    // set our variable
+    //for... of to iterate through the number
+    let newRowArray = [];
+    let newColArray = [];
+    let noOfRow  = matrix[0].length;
+    
+    for (let col = 0; col < noOfRow; col++) {
+      for (const row of matrix) {
+        newRowArray.push(row[col]);
+      }
+      newColArray.push(newRowArray);
+      newRowArray = [];
+    }
+    return newColArray;
+};
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } 
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+module.exports = transpose;
+    
